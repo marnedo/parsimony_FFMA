7d6936f174b96136910ecb23f1eb70d4deff54cc
Miquel Arnedo <marnedo@gmail.com>
2024-09-29 16:59:40 +0200

    First part

---
+++ setup.R
@@ -0,0 +1,5 @@
+knitr::opts_chunk$set(echo = TRUE, 
+                      comment = "#>",
+                      warning = FALSE,
+                      message = FALSE,
+                      collapse = TRUE)
